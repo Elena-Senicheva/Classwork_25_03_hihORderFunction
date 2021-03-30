178d3227c01816816e5ac73648c6eacbc681dd3b
Elena Senicheva <elenasenicheva81@gmail.com>
2021-03-30 12:01:42 +0300

    add new changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,65 @@
-// let counter = 0;
+'use strict';
 
-// while (counter <10 ){
-//    if(counter % 2 === 0){
-//     console.log(counter);
+// const user = {
+//   firstName: 'Test',
+//   lastName: 'Testovich',
+//   age: 18,
+//   isMale: true,
+//   sayHello: function(){
+//     alert(`Hello,my name is ${this.lastName} ${this.firstName}.`);
+    
+//   },
+//   isAdult: function(){
+//    return this.age>= 18;
 //   }
-//   counter++;
 // }
 
+// user.sayHello();
+// console.log(user.isAdult());
 
-const GOOD_PASSWORD = "qwerty123";
 
-let counter=0;
+// const User = function(firstName, lastName , age, isMale){
+// this.firstName = firstName;
+// this.lastName = lastName;
+// this.age = age;
+// this.isMale = isMale;
+// this.sayHello = function(){
+//   alert(`Hello,my name is ${this.lastName} ${this.firstName}.`);
+// }
+// this.isAdult = function(){
+//   return this.age>= 18;
+// }
+// }
 
-while (counter < 5 ){
-  const parol = prompt("Введите пароль:");
-  if(parol != GOOD_PASSWORD){
-    console.log("Пароль введён неверно");
-  }
-  else{
-    break;
-  }
-   counter++ 
+// let user1 = new User('Ivanov', 'Ivan', 15, true,);
+// let user2 = new User('Petrov', 'Petr', 25, true,);
+
+// console.log(user1);
+// console.log(user2);
+// console.log(user1.sayHello());
+
+// const PROTOKOL = 'http://';
+// const SERVER_IP = 'localhost';
+// const PORT = 3000;
+
+// function createQuery(absPath) {
+//   return `${PROTOKOL}${SERVER_IP}:${PORT}${absPath}`;
   
+// }
+
+// console.log(createQuery('/home/opa'));
+
+function createMultiplicationTable(){
+  const table = {
+       
+  }
+
+for(let i=1; i<=10; i++){
+  for(let j=1; j<=10; j++){
+   table[`${i} * ${j}`] = i + j;
+  }
 }
+return table;
+}
+
+console.log(createMultiplicationTable());
